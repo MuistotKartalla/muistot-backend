ec10c9fd92d2a59a729dc8097d913f19de3a2107
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-03-04 10:40:44 +0200

    Changes api models to allow empty strings

--- src/muistoja/backend/models/_fields.py
+++ src/muistoja/backend/models/_fields.py
@@ -44,7 +44,7 @@ This abides ISO 639-1
 LANG_FIELD = Field(description="Language tag, supports ISO 639-1 format")
 
 COUNTRY = constr(
-    min_length=1,
+    min_length=2,
     max_length=6,
     regex=r"^(?:[A-Z]{2,3}|[A-Z]{2}-(?:[A-Z]{1,3}|[0-9]{1,3}))",
 )
@@ -61,7 +61,7 @@ COUNTRY_FIELD = Field(
 LAT = confloat(ge=-90, le=90)
 LON = confloat(ge=-180, le=180)
 
-NAME = constr(strip_whitespace=True, min_length=1, max_length=200)
-TEXT = constr(strip_whitespace=True, min_length=1, max_length=20_000)
-SMALL_TEXT = constr(strip_whitespace=True, min_length=1, max_length=2_500)
-LONG_TEXT = constr(strip_whitespace=True, min_length=1, max_length=100_000)
+NAME = constr(strip_whitespace=True, min_length=0, max_length=200)
+TEXT = constr(strip_whitespace=True, min_length=0, max_length=20_000)
+SMALL_TEXT = constr(strip_whitespace=True, min_length=0, max_length=2_500)
+LONG_TEXT = constr(strip_whitespace=True, min_length=0, max_length=100_000)
