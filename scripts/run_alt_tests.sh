d114b2e0456842bc080c6eabadee2792ed818377
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-04-05 14:25:23 +0300

    Removes unneeded services from tests and disables main workflow

--- scripts/run_alt_tests.sh
+++ scripts/run_alt_tests.sh
@@ -4,7 +4,7 @@ cd "${0%/*}/.."
 docker compose -f test-runner-compose.yml down -v --remove-orphans
 docker compose -f test-runner-compose.yml up \
   --force-recreate --remove-orphans --build \
-  --abort-on-container-exit --exit-code-from runner --attach runner --attach mailer
+  --abort-on-container-exit --exit-code-from runner --attach runner
 test_result=$?
 docker compose -f test-runner-compose.yml down -v --remove-orphans # This can be omitted to leave db on
 if [ "$test_result" = "0" ]; then
