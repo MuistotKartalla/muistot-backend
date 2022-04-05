231df608e9ab03edcff3edc70ca6500b599173f7
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-04-05 10:53:14 +0300

    Root path to servers more hopefully

--- src/muistoja/errors/helpers.py
+++ src/muistoja/errors/helpers.py
@@ -12,6 +12,10 @@ from .models import *
 def modify_openapi(app: FastAPI):
     from pydantic.schema import schema
 
+    # BECAUSE Fastapi does this on first request only
+    if app.root_path is not None and app.root_path != "":
+        app.servers.insert(0, {"url": app.root_path})
+
     openapi = app.openapi()
     openapi["components"]["schemas"].update(
         schema([HTTPValidationError], ref_prefix="#/components/schemas/")[
