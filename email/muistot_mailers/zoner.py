0b018db24e887abb035b1792c958faf66fd8ae72
joniumGit <52005121+joniumGit@users.noreply.github.com>
2022-04-14 22:36:32 +0300

    Updates zoner mailer with register support

--- email/muistot_mailers/zoner.py
+++ email/muistot_mailers/zoner.py
@@ -37,7 +37,7 @@ def set_template_data(subject: str, title: str, button: str, link: str):
 def get_eng_template(user: str, link: str):
     return set_template_data(
         "Muistotkartalla Kirjautuminen",
-        f"Hei {user}! Tässä kirjautumislinkkisi muistotkartalla.fi palveluun.",
+        f"Hei {user}! Tässä kirjautumislinkkisi muistotkartalla palveluun",
         "Kirjaudu Sisään",
         link
     )
@@ -46,7 +46,7 @@ def get_eng_template(user: str, link: str):
 def get_fi_template(user: str, link: str):
     return set_template_data(
         "Muistotkartalla Login",
-        f"Hi {user}! Here is your muistotkartalla.fi login link.",
+        f"Hi {user}! Here is your muistotkartalla login link",
         "Click to Login",
         link
     )
@@ -58,12 +58,13 @@ class ZonerMailer(Mailer):
     def __init__(self, **kwargs):
         self.config = MailerConfig(**kwargs)
 
-    def send_via_smtp(self, email: str, subject: str, content: MIMEText):
+    def send_via_smtp(self, email: str, subject: str, *content: MIMEText):
         mail = MIMEMultipart("alternative")
         mail["Subject"] = subject
         mail["From"] = self.get_sender()
         mail["To"] = email
-        mail.attach(content)
+        for c in content:
+            mail.attach(c)
         with SMTP_SSL(self.config.host, port=self.config.port) as s:
             s.login(self.config.user, self.config.password)
             s.send_message(mail)
@@ -78,10 +79,48 @@ class ZonerMailer(Mailer):
 
         if lang == "en":
             subject = "Muistotkartalla Login"
-            content = MIMEText(get_eng_template(user, url), "html")
+            content = [
+                MIMEText(get_eng_template(user, url), "html"),
+                MIMEText(f"Login link: {url}", "plain")
+            ]
         else:
             subject = "Muistotkartalla Kirjautuminen"
-            content = MIMEText(get_fi_template(user, url), "html")
+            content = [
+                MIMEText(get_fi_template(user, url), "html"),
+                MIMEText(f"Linkki kirjautumiseen: {url}", "plain")
+            ]
+
+        return subject, content
+
+    def handle_verify_data(self, user: str, token: str, verified: bool, lang: str = "fi", **_):
+        from urllib.parse import urlencode
+        url = urlencode(dict(user=user, token=token, verified=f'{bool(verified)}'.lower()))
+        url = f'{self.config.service_url}#verify-user:{url}'
+
+        if lang == "en":
+            subject = "Muistotkartalla Verification"
+            content = [
+                MIMEText(set_template_data(
+                    "Muistotkartalla Verification",
+                    f"Welcome {user}! Please take a moment to verify your account to start using muistotkartalla."
+                    f"Click the button below or enter the code: {token}.",
+                    "Verify Account",
+                    url,
+                ), "html"),
+                MIMEText(f"Verify link: {url}", "plain")
+            ]
+        else:
+            subject = "Muistotkartalla Tilin Vahvistus"
+            content = [
+                MIMEText(set_template_data(
+                    "Muistotkartalla Tilin Vahvistus",
+                    f"Tervetuloa {user}! Vahvista vielä muistotkartalla tilisi klikkaamalla alla olevaa painiketta"
+                    f"tai koodilla {token}.",
+                    "Vahvista Tilisi",
+                    url,
+                ), "html"),
+                MIMEText(f"Linkki tilin vahvistamiseen: {url}", "plain")
+            ]
 
         return subject, content
 
@@ -90,6 +129,8 @@ class ZonerMailer(Mailer):
 
             if email_type == "login":
                 subject, content = self.handle_login_data(**data)
+            elif email_type == "register":
+                subject, content = self.handle_verify_data(**data)
             else:
                 subject = "Muistotkartalla" if "subject" not in data else data["subject"]
                 content = MIMEText(data.get("content", ""))
