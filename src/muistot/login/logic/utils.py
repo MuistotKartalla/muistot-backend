5fbdb1e46f291609f0e4cd88031497eae0ed6ff7
jonium <52005121+joniumGit@users.noreply.github.com>
2022-12-16 12:37:24 +0200

    Reduces login timeout

--- src/muistot/login/logic/utils.py
+++ src/muistot/login/logic/utils.py
@@ -6,5 +6,5 @@ def ratelimit_via_redis_host_and_key(r: Request, key: str):
     if cache.exists(key, r.client.host, prefix="email-login:"):
         raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail="Too many requests")
     else:
-        cache.set(key, "", prefix="email-login:", ttl=60)
-        cache.set(r.client.host, "", prefix="email-login:", ttl=60)
+        cache.set(key, "", prefix="email-login:", ttl=20)
+        cache.set(r.client.host, "", prefix="email-login:", ttl=20)
