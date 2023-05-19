735b8ff6c5c71168f47b0959fc851df0958114ea
joniumGit <52005121+joniumGit@users.noreply.github.com>
2023-05-19 10:24:34 +0300

    More forceful recreate

--- scripts/recreate_db.sh
+++ scripts/recreate_db.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd "${0%/*}/.."
-docker-compose down -v
+docker-compose down -v --remove-orphans
 docker volume rm muistot-db-data
 docker volume create --name muistot-db-data
-docker-compose up -d db
+docker-compose up --force-recreate -d db
